b3feb0215e946d5ad925fcc3a56ac3fbc10b0609
marialuizab11 <marialuizabsantos0@gmail.com>
2025-09-10 17:42:56 -0300

    corrige nome e combinaçães dos quadrados

--- settings.py
+++ settings.py
@@ -5,34 +5,35 @@ FONT_SIZE = 20
 HEADER_HEIGHT = 60
 
 MERGE_RULES = {
-    ("Lógica Matemática", "Lógica Matemática"): ("Matemática Discreta", 10),
-    ("Geometria Analítica", "Geometria Analítica"): ("Álgebra Linear", 10),
-    ("Geometria Analítica", "Cálculo I"): ("Cálculo II", 20),
-    ("Cálculo I", "Geometria Analítica"): ("Cálculo II", 20),
-    ("Introdução à Programação I", "Introdução à Programação I"): ("Algoritmos e Estruturas de Dados I", 10),
-    ("Algoritmos e Estruturas de Dados I", "Programação Orientada a Objetos"): ("Engenharia de Software I", 50),
-    ("Álgebra Linear", "Algoritmos e Estruturas de Dados I"): ("Computação Gráfica", 50),
+    ("IP", "IP"): ("POO", 2),
+    ("POO", "POO"): ("AED I", 4),
+    ("AED I", "AED I"): ("AED II", 8),
+    ("AED II", "AED II"): ("CG", 16),
+    ("CG", "CG"): ("PLP", 32),
+    ("PLP", "PLP"): ("PAA", 64),
+    ("PAA", "PAA"): ("Compiladores", 128),
+    ("Compiladores", "Compiladores"): ("IA", 256),
+    ("IA", "IA"): ("RP", 512),
+    ("RP", "RP"): ("Projetão", 1024),
+    ("Projetão", "Projetão"): ("TCC", 2048),
 }
 
 COLORS = {
-    "Lógica Matemática": (70, 130, 180),
-    "Matemática Discreta": (34, 139, 34),
-    "Geometria Analítica": (255, 140, 0),
-    "Álgebra Linear": (148, 0, 211),
-    "Cálculo I": (255, 215, 0),
-    "Cálculo II": (178, 34, 34),
-    "Introdução à Programação I": (0, 206, 209),
-    "Algoritmos e Estruturas de Dados I": (219, 112, 147),
-    "Programação Orientada a Objetos": (255, 105, 180),
-    "Engenharia de Software I": (105, 105, 105),
-    "Computação Gráfica": (123, 104, 238),
+    "IP": (70, 130, 180),
+    "POO": (34, 139, 34),
+    "AED I": (255, 140, 0),
+    "AED II": (148, 0, 211),
+    "PLP": (255, 215, 0),
+    "PAA": (178, 34, 34),
+    "CG": (0, 206, 209),
+    "IA": (219, 112, 147),
+    "RP": (255, 105, 180),
+    "Projetão": (105, 105, 105),
+    "TCC": (123, 104, 238),
 }
 
 BASIC_POOL = [
-    "Lógica Matemática",
-    "Geometria Analítica",
-    "Cálculo I",
-    "Introdução à Programação I",
+    "IP"
 ]
 
 MAX_FONT_SIZE = 24
